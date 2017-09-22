696a9ffdedbce59c367f247c5359ca552043d5f3
paprikaLang <3060866223@qq.com>
2017-09-22 09:00:56 +0800

    readme

---
+++ README.md
@@ -0,0 +1,13 @@
+## Functional Programming
+
+![](https://ws1.sinaimg.cn/large/006tKfTcgy1fjs0fvb71bj31e40ncmze.jpg)
+
+简单的函数就是给定一个变量和改变变量的参数,得出一个新的变量 例如: y = x + a;
+
+对于函数式编程,就是接收一个已有状态state和改变状态的用户行为Action,得出一个新的state的过程.
+
+对于上面的单向数据流程图,stateDidChanged是一个典型的纯函数式的UI更新方法,reducer同样是,它们非常易于测试,不用关心controller,只需要知道前后两个state就可维护controller了.
+
+函数式编程保证了数据单向流动和完整的状态变更覆盖测试,对于学习RxSwift和前端React等都是很有帮助的
+
+
--- TodoDemoTests/TodoDemoTests.swift
+++ TodoDemoTests/TodoDemoTests.swift
@@ -26,8 +26,47 @@ class TodoDemoTests: XCTestCase {
         controller = nil
         super.tearDown()
     }
+    func testUpdateView(){
+        let state1 = TableViewController.State(
+            dataSource: TableViewDataSource(todos:[], owner: nil),
+            text:""
+        )
+        //从nil状态转换为state1
+        controller.stateDidChanged(state: state1, previousState: nil, command: nil)
+        XCTAssertEqual(controller.tableView.numberOfRows(inSection: TableViewDataSource.Section.todos.rawValue), 0)
+        XCTAssertFalse(controller.navigationItem.rightBarButtonItem!.isEnabled)
+        
+        let state2 = TableViewController.State(dataSource: TableViewDataSource(todos:["1","3"],owner:nil), text: "helloWorld")
+        //从state1转换到state2
+        controller.stateDidChanged(state: state2, previousState: state1, command: nil)
+        XCTAssertEqual(controller.tableView.numberOfRows(inSection: TableViewDataSource.Section.todos.rawValue), 2)
+        XCTAssertEqual(controller.tableView.cellForRow(at: todoItemIndexPath(row: 1))?.textLabel?.text, "3")
+        XCTAssertTrue(controller.navigationItem.rightBarButtonItem!.isEnabled)
     
-  
+    }
+    
+    func testReducerUpdateTextFromEmpty(){
+        let initState = TableViewController.State()
+        let state = controller.reducer(initState, .updateText(text: "123")).state
+        XCTAssertEqual(state.text, "123")
+    }
+   
+    func testLoadTodos(){
+        
+        let initState = TableViewController.State()
+        let(_,command) = controller.reducer(initState, .loadToDos)
+        XCTAssertNotNil(command)
+        switch command! {
+        case .loadToDos(let handler):
+            handler(["2","3"])
+            XCTAssertEqual(controller.store.state.dataSource.todos, ["2","3"])
+        default:
+            XCTFail("The command should be .loadToDos")
+        }
+    }
+}
+func todoItemIndexPath(row: Int) -> IndexPath {
+    return IndexPath(row: row, section: TableViewDataSource.Section.todos.rawValue)
 }
 
 
